45245367979461ddd93a37cee14f9f5475ee3ee8
emperormesh <abochichinonso@gmail.com>
2021-03-25 03:00:41 -0700

    modifies sign up button

---
+++ index.js
@@ -0,0 +1,44 @@
+function validateName(value) {
+	const regEx = /^[A-Za-z]+((\s)?([A-Za-z])+)*$/;
+	const isNameValid = regEx.test(value);
+	return isNameValid
+}
+
+function validateEmail(value) {
+	const emailRegExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const isEmailValid = emailRegExp.test(value)
+	return isEmailValid
+}
+function passwordValidation(value) {
+	const passwordRegexp = /^[A-Za-z]\w{7,14}$/;
+	const isPasswordValid = passwordRegexp.test(value);
+	return isPasswordValid
+}
+
+function Submit(event) {
+	const firstname = $('#firstname').val()
+	const lastname = $('#lastname').val()
+	const email = $('#email').val()
+	const password = $('#password').val()
+	const password2 = $('#password2').val()
+	
+
+	if(!validateName(firstname)){
+		return alert(` Invalid input '${firstname}'firstname`)
+	}
+	
+	if(!validateName(lastname)){
+		alert(` Invalid Input for${lastname}' Lastname`)
+	}
+
+	if(!validateEmail(email)){
+		alert(` Please Enter a ${email} Correct Email`)
+	}
+	if(!passwordValidation(password)){
+		alert(`${password} PLease input a strong password`)
+	}
+	if(password2 !== password ){
+		alert("Password do not match")
+	
+	}
+}
