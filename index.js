736f9b13538838670f944105fb020e0a69e4c08a
unknown <oracle10gee@gmail.com>
2021-03-25 11:27:13 +0100

    added a gitignore file

---
+++ index.js
@@ -0,0 +1,22 @@
+// function validateFirstName(value){
+//     const regEx = /^[a-z,''.]/w/i
+//     const isFirstname= regEx.test(value)
+//     return isFirstname
+// }
+// function validateLastName(value){
+//     con
+// }
+// function submit(event){
+//     event.preventDefault()
+//     const firstname = document.getElementById("firstname");
+// const lastname = document.getElementById("lastname");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const password2 = document.getElementById("password2");
+// }
+
+var sum = 0;
+function addSum(num) {
+  sum = sum + num;
+}
+addSum(3);
